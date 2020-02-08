f23b1fffaa334b3e0cc9cd83cd6a209eab213caa
geonamrata <60631229+geonamrata@users.noreply.github.com>
2020-02-08 18:26:35 -0500

    save

--- myStrTest.py
+++ myStrTest.py
@@ -4,3 +4,14 @@ if name.endswith("G"):
     print (addName)
 else:
     print (name+ "Acharya")
+
+
+name= ["NamrataG", "SampadaA", "SagunA", "SharadA"]
+if name[0].endswith('G'):
+    name1= name[0][:-1]+ " Guragain"
+    name.append(name1)
+    print(name)
+else:
+    print(name)
+
+
